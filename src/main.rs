53128517444727a20466f0cd8b3d0afbb7bc036c
hrbrmstr <bob@rud.is>
2022-01-28 13:48:05 -0500

    reindent

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,15 @@ use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
   
-  {
-    let mut buf = [0; 1024];
-    let s = UdpSocket::bind("0.0.0.0:50222").expect(r#"{"message":"Could not bind to address/port."}"#);
+  let mut buf = [0; 1024];
+  let s = UdpSocket::bind("0.0.0.0:50222").expect(r#"{"message":"Could not bind to address/port."}"#);
+  
+  loop {
     
-    loop {
-      
-      let (n, _) = s.recv_from(&mut buf).expect(r#"{"message":"No broadcasts received."}"#);
-      
-      println!("{}", String::from_utf8(buf[..n].to_vec()).unwrap())
-      
-    }
+    let (n, _) = s.recv_from(&mut buf).expect(r#"{"message":"No broadcasts received."}"#);
+    
+    println!("{}", String::from_utf8(buf[..n].to_vec()).unwrap())
+    
   }
   
 }
