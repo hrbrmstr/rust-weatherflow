4e4c64c49a728e4cb4566b333af12490c5d9d20d
hrbrmstr <bob@rud.is>
2022-01-28 13:46:27 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+  
+  {
+    let mut buf = [0; 1024];
+    let s = UdpSocket::bind("0.0.0.0:50222").expect(r#"{"message":"Could not bind to address/port."}"#);
+    
+    loop {
+      
+      let (n, _) = s.recv_from(&mut buf).expect(r#"{"message":"No broadcasts received."}"#);
+      
+      println!("{}", String::from_utf8(buf[..n].to_vec()).unwrap())
+      
+    }
+  }
+  
+}
