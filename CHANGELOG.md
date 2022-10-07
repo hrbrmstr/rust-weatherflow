b5124d2210ac46ff8ae2c8fb140682df5d8a4a09
hrbrmstr <bob@rud.is>
2022-10-07 08:03:34 -0400

    feat: added ability to use a different port to future proof the utility
    chore: release 0.2.1
    
    Release-As: 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,5 +6,3 @@
 ### Miscellaneous Chores
 
 * release 0.2.0 ([7cd60ba](https://github.com/hrbrmstr/rust-weatherflow/commit/7cd60bade9e92f646f433d04c92c3947f36f30b5))
-* release 0.2.0 ([6f58dff](https://github.com/hrbrmstr/rust-weatherflow/commit/6f58dffe1801d673f0b69970db16f1065a327b24))
-* release 0.2.0 ([25c462b](https://github.com/hrbrmstr/rust-weatherflow/commit/25c462b4d8cc01e5054b9a250c4f4ae6d3667f84))
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-
+	// which port to listen on; weatherflow tempest presently defaults to 50222 but that may change
+  #[clap(long, default_value_t = String::from("50222"))]
+  port: String,
 }
 
 fn main() -> std::io::Result<()> {
@@ -12,11 +14,13 @@ fn main() -> std::io::Result<()> {
   let _args = Args::parse();
 
   let mut buf = [0; 1024];
-  let s = UdpSocket::bind("0.0.0.0:50222").expect(r#"{"message":"Could not bind to address/port."}"#);
+  let s = UdpSocket::bind(format!("0.0.0.0:{}", _args.port))
+	  .expect(r#"{"message":"Could not bind to address/port."}"#);
   
   loop {
     
-    let (n, _) = s.recv_from(&mut buf).expect(r#"{"message":"No broadcasts received."}"#);
+    let (n, _) = s.recv_from(&mut buf)
+		  .expect(r#"{"message":"No broadcasts received."}"#);
     
     println!("{}", String::from_utf8(buf[..n].to_vec()).unwrap())
     
